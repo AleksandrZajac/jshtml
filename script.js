997668d400c1963ba9e796b323b9791e984c0b6d
Aleksandr Zajac <mr.goodalex@gmail.com>
2020-09-20 11:08:01 +0300

    Six commit

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('Hello world!!!');
+var wizards = [
+  {
+    name: 'Пендальф',
+    eyesColor: 'blue',
+    coatColor: 'green'
+  },
+  {
+    name: 'Баба Яга',
+    eyesColor: 'black',
+    coatColor: 'gray'
+  },
+  {
+    name: 'Сарумян',
+    eyesColor: 'white',
+    coatColor: 'yellow'
+  },
+  {
+    name: 'Радагаст',
+    eyesColor: 'red',
+    coatColor: 'blue'
+  }
+];
+
+var party = 'Саурон пригласил на вечеринку: ';
+
+var result = wizards.
+  map(function (it) {
+    return it.coatColor;
+  }).join(', ');
+
+console.log(party + result);
